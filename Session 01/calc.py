0c39c9a051501ead55950d461427b642f7aa5f56
atsung1 <atsung1@babson.edu>
2018-08-31 22:43:25 -0400

    Exercises 1 & 2

---
+++ Session 01/calc.py
@@ -0,0 +1,15 @@
+#question 1:2562s
+print(42*60+42)
+#question 2:6.21miles
+print(10/1.61)
+#question 3:
+a=42*60+4
+b=10/1.61
+#time per mile in seconds:406.364
+print(a/b)
+#time per mile in minutes:6.772
+c=a/b
+print(c/60)
+#average speed in miles per hour: 8.859
+d=a/60/60
+print(b/d)
---
+++ Session 01/ex1.py
@@ -0,0 +1,12 @@
+#exercise 1
+print Hello World
+#question 1: doesn't allow for only 1 parentheses, returns syntax error
+print (Hello World)
+#question 2: Error syas object not found, since it's not recognized as a string without quotation marks
+print (+2)
+print (2++2)
+#question 3: a plus sign before a number doesn't change it; 2++2 = 4
+print (02)
+#question 4: adding a 0 in front doesn't work in Python
+print (2 2)
+#question 5: syntax error
